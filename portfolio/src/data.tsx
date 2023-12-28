63e930d73e4b77a73d70a6f433b0626555551990
Lucas Mateus <lucasmfpiu@gmail.com>
2023-12-28 06:10:55 -0300

    Mudança padrão

--- portfolio/src/data.tsx
+++ portfolio/src/data.tsx
@@ -5,7 +5,7 @@ const banners = [{
     title:"Thanks",
     text:"Quero expressar minha gratidão por ter dedicado um tempo para visitar o meu perfil. Fiquei muito contente em saber do seu interesse. Se houver qualquer informação adicional que você gostaria de obter ou se tiver alguma pergunta, sinta-se à vontade para entrar em contato. lucas314u@gmail.com   "
 }]
-https://le314u.github.io/games/telma/dist/index.html
+
 let games = {cards:[
     {
         title:"Telma",
