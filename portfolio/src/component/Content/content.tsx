315757ef2e2395e83c32d39b2f7adbf7ce0427bb
Lucas Mateus <lucasmfpiu@gmail.com>
2023-12-28 16:54:47 -0300

    img and href

--- portfolio/src/component/Content/content.tsx
+++ portfolio/src/component/Content/content.tsx
@@ -7,40 +7,45 @@ import "materialize-css/dist/js/materialize.min.js";
 interface ContentProps {
   text: string;
   title: string;
+  href:string;
   imageUrl: string;
   imageAlt: string;
   id: string; // ou o tipo apropriado para o ID, dependendo do seu caso
 }
 
-const Content : React.FC<ContentProps> = ({ text, title, imageUrl, imageAlt,id }) => {
+const Content : React.FC<ContentProps> = ({ text, title,href, imageUrl, imageAlt,id }) => {
   return (
     <div id={id} className="component">
-      <div className="content"style={{ display: 'flex', alignItems: 'center' }}>
-        <div className="txt" style={{ flex: '1', marginRight: '20px' }}>
-          <p className='title'>{title}</p>
-          <p className='text'>{text}</p>
+      <a href={href}>
+        <div className="content"style={{ display: 'flex', alignItems: 'center' }}>
+          <div className="txt" style={{ flex: '1', marginRight: '20px' }}>
+            <p className='title'>{title}</p>
+            <p className='text'>{text}</p>
+          </div>
+          <div className="img"  style={{ flex: '1' }}>
+            <img src={imageUrl} alt={imageAlt} style={{ width: '100%', height: 'auto' }} />
+          </div>
         </div>
-        <div className="img"  style={{ flex: '1' }}>
-          <img src={imageUrl} alt={imageAlt} style={{ width: '100%', height: 'auto' }} />
-        </div>
-      </div>
+      </a>
     </div>
   );
 };
 
 
-const InverseContent : React.FC<ContentProps>  = ({ text, title, imageUrl, imageAlt, id }) => {
+const InverseContent : React.FC<ContentProps>  = ({ text, title,href, imageUrl, imageAlt, id }) => {
   return (
     <div id={id} className="component">
-      <div className="inversecontent"style={{ display: 'flex', alignItems: 'center' }}>
-        <div className="img"  style={{ flex: '1' }}>
-          <img src={imageUrl} alt={imageAlt} style={{ width: '100%', height: 'auto' }} />
+      <a href={href}>
+        <div className="inversecontent"style={{ display: 'flex', alignItems: 'center' }}>
+          <div className="img"  style={{ flex: '1' }}>
+            <img src={imageUrl} alt={imageAlt} style={{ width: '100%', height: 'auto' }} />
+          </div>
+          <div className="txt" style={{ flex: '1', marginRight: '20px' }}>
+            <p className='title'>{title}</p>
+            <p className='text'>{text}</p>
+          </div>
         </div>
-        <div className="txt" style={{ flex: '1', marginRight: '20px' }}>
-          <p className='title'>{title}</p>
-          <p className='text'>{text}</p>
-        </div>
-      </div>
+      </a>
     </div>
   );
 };
