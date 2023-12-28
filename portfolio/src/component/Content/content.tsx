eda1dd248fdf099c5f766ad48127c83b55f8149b
Lucas Mateus <lucasmfpiu@gmail.com>
2023-12-27 21:29:09 -0300

    portifolio

---
+++ portfolio/src/component/Content/content.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import './content.css'
+import "materialize-css/dist/css/materialize.min.css";
+import "materialize-css/dist/js/materialize.min.js";
+
+
+interface ContentProps {
+  text: string;
+  imageUrl: string;
+  imageAlt: string;
+  id: string; // ou o tipo apropriado para o ID, dependendo do seu caso
+}
+
+const Content : React.FC<ContentProps> = ({ text, imageUrl, imageAlt,id }) => {
+  return (
+    <div id={id} className="component">
+      <div className="content"style={{ display: 'flex', alignItems: 'center' }}>
+        <div className="txt" style={{ flex: '1', marginRight: '20px' }}>
+          <p>{text}</p>
+        </div>
+        <div className="img"  style={{ flex: '1' }}>
+          <img src={imageUrl} alt={imageAlt} style={{ width: '100%', height: 'auto' }} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
+const InverseContent : React.FC<ContentProps>  = ({ text, imageUrl, imageAlt, id }) => {
+  return (
+    <div id={id} className="component">
+      <div className="inversecontent"style={{ display: 'flex', alignItems: 'center' }}>
+        <div className="img"  style={{ flex: '1' }}>
+          <img src={imageUrl} alt={imageAlt} style={{ width: '100%', height: 'auto' }} />
+        </div>
+        <div className="txt" style={{ flex: '1', marginRight: '20px' }}>
+          <p>{text}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
+
+
+
+
+
+
+
+
+
+export {Content,InverseContent};
---
+++ portfolio/src/component/Footer/footer.tsx
@@ -0,0 +1,29 @@
+import './footer.css'
+
+
+function Footer() {
+
+  return (
+    <footer className="footerBar">
+
+      <i className="icon"/>
+      <div className="footerBar-column"><ul>
+        <li>Textão de textao</li>
+        <li>Textão de textao</li>
+        <li>Textão de textao</li>
+      </ul></div>
+      <div className="footerBar-column"><ul>
+        <li>Game de Game</li>
+        <li>Game de Game</li>
+        <li>Game de Game</li>
+      </ul></div>
+      <div className="footerBar-column"><ul>
+        <li>Magic</li>
+        <li>Magic</li>
+        <li>Magic</li>
+      </ul></div>
+    </footer>
+  )
+}
+
+export default Footer
