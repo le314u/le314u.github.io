2342527413133139753638044a993c45e4c17467
Lucas Mateus <lucasmfpiu@gmail.com>
2023-12-27 21:43:01 -0300

    BUILD

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/portfolio/dist', // ou '/Games/perfil/dist/' se necessário
   plugins: [react()],
 })
