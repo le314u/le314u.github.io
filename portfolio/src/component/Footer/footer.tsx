64406300da04badb58aac075d45954087192fc47
Lucas Mateus <lucasmfpiu@gmail.com>
2023-12-28 06:18:51 -0300

    Mudança padrão

--- portfolio/src/component/Footer/footer.tsx
+++ portfolio/src/component/Footer/footer.tsx
@@ -1,36 +1,8 @@
 import './footer.css'
 
-
-function Footer(props:any) {
-
+function Logos(){
   return (
-    <footer id={props.id} className="footerBar">
-
-      <i className="icon"/>
-      <div className="footerBar-column"><ul>
-        <li></li>
-        <li>Jiu-Jitsu</li>
-        <li>Capoeira</li>
-        <li>Musculação</li>
-      </ul></div>
-      <div className="footerBar-column"><ul>
-        <li>Game</li>
-        <li>Telma</li>
-        <li>Perfil</li>
-        <li>Who is Spy?</li>
-      </ul></div>
-      <div className="footerBar-column"><ul>
-        <li>cEDH</li>
-        <li>Orvar</li>
-        <li>Urza</li>
-      </ul></div>
-      <div className="footerBar-column"><ul>
-        <li>Legacy</li>
-        <li>UWx</li>
-        <li>HighTide</li>
-      </ul></div>
-      <div>
-      <div style={{display:'block'}}>
+    <div className="logos" style={{display:'block'}}>
         <img alt='js' loading="lazy"  width="40" height="40" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"/>
         <img alt='react' loading="lazy"  width="40" height="40" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />
         <img alt='electron' loading="lazy"  width="40" height="40" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/electron/electron-original.svg" />
@@ -44,7 +16,38 @@ function Footer(props:any) {
         <img alt='vsCode' loading="lazy"  width="40" height="40" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg" />
         <img alt='linux' loading="lazy"  width="40" height="40"  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg" /> 
       </div>
+  )
+}
+
+function Footer(props:any) {
+  return (
+    <footer id={props.id} className="footerBar">
+      <div>
+        <i className="icon"/>
+        <div className="footerBar-column"><ul>
+          <li></li>
+          <li>Jiu-Jitsu</li>
+          <li>Capoeira</li>
+          <li>Musculação</li>
+        </ul></div>
+        <div className="footerBar-column"><ul>
+          <li>Game</li>
+          <li>Telma</li>
+          <li>Perfil</li>
+          <li>Who is Spy?</li>
+        </ul></div>
+        <div className="footerBar-column"><ul>
+          <li>cEDH</li>
+          <li>Orvar</li>
+          <li>Urza</li>
+        </ul></div>
+        <div className="footerBar-column"><ul>
+          <li>Legacy</li>
+          <li>UWx</li>
+          <li>HighTide</li>
+        </ul></div>
       </div>
+      <Logos/>
     </footer>
   )
 }
