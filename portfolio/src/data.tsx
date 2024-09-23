1feb7d01255a1d389b82a5782df76e394e2ec86b
Le314u <lucasmfpiu@gmail.com>
2024-09-23 04:37:12 -0300

    portifolio

--- portfolio/src/data.tsx
+++ portfolio/src/data.tsx
@@ -1,3 +1,11 @@
+import tcc from "./assets/img/projetos/tcc.gif"
+import proxy from "./assets/img/projetos/proxy.gif"
+import layout from "./assets/img/projetos/layout.gif"
+import juros from "./assets/img/projetos/juros.gif"
+import academico from "./assets/img/projetos/academico.png"
+
+
+
 const banners = [{
     title:"Le314u",
     text:"Olá! Meu nome é Lucas Mateus vulgarmente conhecido como piu, uma pessoa com uma paixão diversificada por conhecimento e experiências. Formado em Licenciatura em Educação Física e Bacharel em Ciências da Computação e uma paixão por Jiujitsu, Magic e Jogos no geral, minha jornada tem sido marcada por uma mistura única de disciplinas."
@@ -33,31 +41,31 @@ const contents = [{
     title:"TCC",
     text:"Ferramenta de visão computacional para a análise do movimento de barra fixa voltada para testes de aptidão física",
     imageAlt:"Gif Tcc",
-    imageUrl:"/public/img/projetos/tcc.gif",
+    imageUrl:tcc,
     href:"https://github.com/le314u/TCC",
 },{
     title:"proxyCardMTG",
     text:"Cria um pdf para impressão de proxys apartir da url de um deck postado no site 'ligamagic'",
     imageAlt:"Gif Scraping",
-    imageUrl:"/public/img/projetos/proxy.gif",
+    imageUrl:proxy,
     href:"https://github.com/le314u/proxyCardMTG",
 },{
     title:'layoutImg',
     text:"Junta duas Imagens em uma unica imagem, otimo para criação de fotos padronizadas e emissão de certificado em png",
     imageAlt:"Gif layout",
     href:"https://github.com/le314u/layoutImg",
-    imageUrl:"/public/img/projetos/layout.gif",
+    imageUrl:layout
 },{
     title:"Financial-Transaction-Manager",
     text:"Facilitar o cadastramento de movimentação de cheques em eventos no google agenda",
     imageAlt:"Gif calculo juros",
-    imageUrl:"/public/img/projetos/juros.gif",
+    imageUrl:juros,
     href:"https://github.com/le314u/financialTransactionManager",
 },{
     title:"Meus trabalhos academicos",
     text:"Trabalhos realizados durante a graduação em Ciencia da Computação pelo IFMG - Campus Formiga",
     imageAlt:"Trabalhos Academico",
-    imageUrl:"/public/img/projetos/academico.png",
+    imageUrl:academico,
     href:"https://github.com/le314u/Academico/tree/master/IFMG",
 },]
 
