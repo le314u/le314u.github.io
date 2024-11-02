55e40e6a4ecf63425b9f4b7bc2771dace22f5945
Lucas <lucasmfpiu@gmail.com>
2024-11-02 03:37:12 -0300

    Semantic UI

---
+++ games/perfil/.vite/deps/chunk-FU566CA5.js
@@ -0,0 +1,90 @@
+import {
+  __commonJS
+} from "./chunk-ACBJSMGG.js";
+
+// node_modules/exenv/index.js
+var require_exenv = __commonJS({
+  "node_modules/exenv/index.js"(exports, module) {
+    (function() {
+      "use strict";
+      var canUseDOM = !!(typeof window !== "undefined" && window.document && window.document.createElement);
+      var ExecutionEnvironment = {
+        canUseDOM,
+        canUseWorkers: typeof Worker !== "undefined",
+        canUseEventListeners: canUseDOM && !!(window.addEventListener || window.attachEvent),
+        canUseViewport: canUseDOM && !!window.screen
+      };
+      if (typeof define === "function" && typeof define.amd === "object" && define.amd) {
+        define(function() {
+          return ExecutionEnvironment;
+        });
+      } else if (typeof module !== "undefined" && module.exports) {
+        module.exports = ExecutionEnvironment;
+      } else {
+        window.ExecutionEnvironment = ExecutionEnvironment;
+      }
+    })();
+  }
+});
+
+// node_modules/warning/warning.js
+var require_warning = __commonJS({
+  "node_modules/warning/warning.js"(exports, module) {
+    "use strict";
+    var __DEV__ = true;
+    var warning = function() {
+    };
+    if (__DEV__) {
+      printWarning = function printWarning2(format, args) {
+        var len = arguments.length;
+        args = new Array(len > 1 ? len - 1 : 0);
+        for (var key = 1; key < len; key++) {
+          args[key - 1] = arguments[key];
+        }
+        var argIndex = 0;
+        var message = "Warning: " + format.replace(/%s/g, function() {
+          return args[argIndex++];
+        });
+        if (typeof console !== "undefined") {
+          console.error(message);
+        }
+        try {
+          throw new Error(message);
+        } catch (x) {
+        }
+      };
+      warning = function(condition, format, args) {
+        var len = arguments.length;
+        args = new Array(len > 2 ? len - 2 : 0);
+        for (var key = 2; key < len; key++) {
+          args[key - 2] = arguments[key];
+        }
+        if (format === void 0) {
+          throw new Error(
+            "`warning(condition, format, ...args)` requires a warning message argument"
+          );
+        }
+        if (!condition) {
+          printWarning.apply(null, [format].concat(args));
+        }
+      };
+    }
+    var printWarning;
+    module.exports = warning;
+  }
+});
+
+export {
+  require_exenv,
+  require_warning
+};
+/*! Bundled license information:
+
+exenv/index.js:
+  (*!
+    Copyright (c) 2015 Jed Watson.
+    Based on code that is Copyright 2013-2015, Facebook, Inc.
+    All rights reserved.
+  *)
+*/
+//# sourceMappingURL=chunk-FU566CA5.js.map
